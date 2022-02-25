daf7a54ab2125cd7d217e5abd786b917738611b2
Arjun Vellanki <arjunv905@gmail.com>
2022-02-25 00:14:48 -0500

    Implemented Spotify authentication

--- generator.py
+++ generator.py
@@ -1,15 +1,16 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth #SpotifyClientCredentials
 import os
 import sys
 
 #----------------------------------[Fields]---------------------------------------------
-#os.environ['SPOTIPY_CLIENT_ID'] = '7e7e604745fa44c5a27798804efaadd8'
-#os.environ['SPOTIPY_CLIENT_SECRET'] = 'cbb4ccc76a344180bfd92fbd0fee6d70'
+os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8888'
+cacheFolder = os.path.join(sys.path[0], ".cache")
 
 # For printing colored text
 def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
 def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
 
 #----------------------------------[Methods]--------------------------------------------
 # Read ClientID and ClientSecret from config.txt
@@ -68,19 +69,32 @@ def readCreds() -> bool:
 
 
 #------------------------------------[Main]---------------------------------------------
+# Checking if the credentials have been stored
 if (not readCreds()):
     prRed("Error: Could not read credentials from config.txt")
     input("Press Enter to exit...")
     sys.exit()
 
 # Using the credentials to create a spotipy object
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
-print("Successfully authenticated with Spotify...")
+cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=cacheFolder)
+auth_manager = SpotifyOAuth(client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET'], cache_handler=cache_handler)
+#client_credentials_manager = SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET'], cache_handler=cache_handler)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+# Ensuring valid credentials were obtained
+try:
+    username = sp.current_user()['id']
+    prGreen("Successfully logged in as: ")
+    prYellow("\t" + username + "\n")
+except spotipy.oauth2.SpotifyOauthError:
+    prRed("Error: Could not log in")
+    input("Press Enter to exit...")
+    sys.exit()
 
 
 
 results = sp.search(q='artist:Sabai', type='track')
 items = results['tracks']['items']
 for i, t in enumerate(items):
-    print("%d %s" % (i, t['name']))
+    print("%d %s" % (i + 1, t['name']))
     print(items[i]['id'])
