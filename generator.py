6c353ca5608d03286d3b04ba70649fb228f855ef
Arjun Vellanki <arjunv905@gmail.com>
2022-02-25 13:26:17 -0500

    Added playlist modifications

--- generator.py
+++ generator.py
@@ -8,9 +8,10 @@ os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8888'
 cacheFolder = os.path.join(sys.path[0], ".cache")
 
 # For printing colored text
-def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
-def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
-def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def strRed(str): return("\033[91m {}\033[00m".format(str))
+def strGreen(str): return("\033[92m {}\033[00m".format(str))
+def strYellow(str): return("\033[93m {}\033[00m".format(str))
+def strCyan(str): return("\033[96m {}\033[00m".format(str))
 
 #----------------------------------[Methods]--------------------------------------------
 # Read ClientID and ClientSecret from config.txt
@@ -71,29 +72,37 @@ def readCreds() -> bool:
 #------------------------------------[Main]---------------------------------------------
 # Checking if the credentials have been stored
 if (not readCreds()):
-    prRed("Error: Could not read credentials from config.txt")
+    print(strRed("Error: Could not read credentials from config.txt"))
     input("Press Enter to exit...")
     sys.exit()
 
 # Using the credentials to create a spotipy object
+scope = 'playlist-modify-private'
 cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=cacheFolder)
-auth_manager = SpotifyOAuth(client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET'], cache_handler=cache_handler)
+auth_manager = SpotifyOAuth(client_id=os.environ['SPOTIPY_CLIENT_ID'], client_secret=os.environ['SPOTIPY_CLIENT_SECRET'], cache_handler=cache_handler, scope=scope)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 # Ensuring valid credentials were obtained
 try:
     username = sp.current_user()['id']
-    prGreen("Successfully logged in as: ")
-    prYellow("\t" + username + "\n")
+    print("\nSuccessfully logged in as: ")
+    print("\t" + strCyan(username) + "\n")
 except spotipy.oauth2.SpotifyOauthError:
-    prRed("Error: Could not log in")
+    print(strRed("Error: Could not authenticated with Spotify, please ensure that valid credentials are stored in config.txt"))
     input("Press Enter to exit...")
     sys.exit()
 
 
+playlist = "https://open.spotify.com/playlist/6rcoePgRt6SaPQ3uKZdAKo?si=f270993300624a31"
 
 results = sp.search(q='artist:Sabai', type='track')
 items = results['tracks']['items']
 for i, t in enumerate(items):
     print("%d %s" % (i + 1, t['name']))
     print(items[i]['id'])
+    print(items[i]['uri'] + "\n")
+
+    # Testing playlist adding
+    sp.playlist_add_items(playlist, [items[i]['uri']], position=None)
+    print(strGreen("Added \"" + t['name'] +"\" to playlist \n"))
+
